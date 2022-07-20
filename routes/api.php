0a9a4126d73c2d516459878ac39fafa3cf9f6489
evan <evan258@Rahayus-MacBook-Air.local>
2022-07-20 20:16:22 +0700

    testing update from mac

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+// tes push
 Route::prefix('headline')->group(function (){
     Route::get('/detik', [HeadlineController::class, 'detik']);
     Route::get('/kompas', [HeadlineController::class, 'kompas']);
