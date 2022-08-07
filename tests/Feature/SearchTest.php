24f0614003a79380dfb4636c367ba55131ce22fc
Evan Haryo <evanharyo258@gmail.com>
2022-08-07 15:16:50 +0700

    add search unit test and unread the example

---
+++ tests/Feature/SearchTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\SearchTest;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SearchTest extends TestCase
+{
+    
+    const baseUrl = [
+        'detikSearch' => '/api/search/detik/bola',
+    ];
+
+    const resultExpected = [
+        'status' => true,
+        'message' => 'Resources fetched',
+        'data' => array(),
+    ];
+
+    public function testDetikSearchSuccess()
+    {
+        $response = $this->get(self::baseUrl['detikSearch']);
+        $response->assertStatus(200)->assertJson(self::resultExpected);
+    }
+}
